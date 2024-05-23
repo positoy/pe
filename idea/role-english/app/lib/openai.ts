209b348dcc55f15f2f6e5d803ccbef809e123d58
안창규 <changkyu.ahn@navercorp.com>
2024-05-23 19:39:20 +0900

    [positoy/pe#3] openai 호출 구현 완료

--- idea/role-english/app/lib/openai.ts
+++ idea/role-english/app/lib/openai.ts
@@ -1,11 +1,8 @@
-import fs from "fs";
 import OpenAI from "openai";
-import EXAMPLES from "./EXAMPLES";
+import PROMPTS from "./PROMPTS";
 
 const openai = new OpenAI({
-  apiKey: fs
-    .readFileSync(`${__dirname}/../../../../../openai/apikey`)
-    .toString(),
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const OPENAI_MODEL = "gpt-4o";
@@ -16,51 +13,9 @@ const queryEnglishSentences = async (
   occupation: string,
   proficiency: string
 ) => {
-  console.log(
-    `fetching openai api... occupation:${occupation}, proficiency:${proficiency}`
-  );
   const completion = await openai.chat.completions.create({
     messages: [
-      {
-        role: "system",
-        content:
-          "You are an English assistant who helps users learn English. When the user enters their occupation and English proficiency, show them example sentences that could be used in a professional environment. ",
-      },
-      {
-        role: "user",
-        content:
-          "When users enter their job and English skills, you automatically generate sentences that can be used in job situations. The sentences created at this time are created according to the three English proficiency levels.\n \
-              beginner: English level of American kindergarten students\n \
-              intermediate: English level of elementary school students in the United States\n \
-              advanced: English level of American high school students\n \
-              expert: English level of American college students",
-      },
-      {
-        role: "user",
-        content:
-          "The response must include the role that can be assigned to the user, five steps to achieve the purpose of the role, and five specific situations that may occur in each step. Lastly, in utterances, create 5 example sentences that can be used in each situation.\n \
-              role: Role that can be given to the user\n \
-              steps: Steps to achieve the purpose of the role\n \
-              situations: specific situations that may occur at each stage\n \
-              utterance: English sentences that may occur during work in each situation",
-      },
-      {
-        role: "user",
-        content: JSON.stringify(EXAMPLES.job_sentence.input),
-      },
-      {
-        role: "assistant",
-        content: JSON.stringify(EXAMPLES.job_sentence.input),
-      },
-      {
-        role: "user",
-        content:
-          "Now, I will suggest you another request.\
-            My occupation and English proficiency will be suggested.\
-            You respond me with 1 role, 5 steps, 5 situations for each step, 5 utterances for each situation in JSON format. total 125 utterances are expected.\
-            And make it sure the response is perfectly JSON formatted.\
-            The response must be in JSON format.",
-      },
+      ...PROMPTS.ask_for_english_job_sentences,
       {
         role: "user",
         content: JSON.stringify({ occupation, proficiency }),
@@ -76,14 +31,11 @@ const getCachedEnglishSetence = async (
   occupation: string,
   proficiency: string
 ) => {
-  console.log(occupation, proficiency);
-  console.log("getCachedEnglishSetence");
   const hash = `english_sentence:${occupation}:${proficiency}`;
-  return hash;
   if (!CACHE.hasOwnProperty(hash)) {
     CACHE[hash] = await queryEnglishSentences(occupation, proficiency);
   }
   return CACHE[hash];
 };
 
-export { getCachedEnglishSetence, queryEnglishSentences };
+export { getCachedEnglishSetence };
